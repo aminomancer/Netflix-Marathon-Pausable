bfb0e97ae5c47ff181a9691b5ed7bc4af778cdc2
Shane Hughes <shughes@mozilla.com>
2023-07-22 23:08:46 -0700

    5.7.2 - skip preroll ads on Starz

--- marathon.user.js
+++ marathon.user.js
@@ -10,7 +10,7 @@
 // @name:ru            Netflix Marathon (пауза)
 // @name:hi            नेटफ्लिक्स मैराथन (रोकने योग्य)
 // @namespace          https://github.com/aminomancer
-// @version            5.7.1
+// @version            5.7.2
 // @description        A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:en     A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:zh-CN  一个可配置的脚本，可自动跳过重述、介绍、演职员表和广告，并点击 Netflix、Amazon Prime Video、Hulu、HBO Max、Starz、Disney+ 和 Hotstar 上的“下一集”提示。 可自定义的热键暂停/恢复自动跳过功能。 Alt + N 进行设置。
@@ -658,6 +658,10 @@ const methods = {
         ) {
           // next episode - this is the only one I know of
           this.clk(store);
+        } else if (this.qry(".preroll-prefix-container")) {
+          // skip to the end of the preroll ad
+          const video = document.querySelector("starz-video video");
+          video.currentTime = video.duration;
         }
       }
     } else {
