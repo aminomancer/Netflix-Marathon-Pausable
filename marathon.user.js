be427dd713730d31986726a41157102c591441d1
Shane Hughes <shughes@mozilla.com>
2023-08-10 17:48:55 -0700

    fix: an annoying error message

--- marathon.user.js
+++ marathon.user.js
@@ -10,7 +10,7 @@
 // @name:ru            Netflix Marathon (пауза)
 // @name:hi            नेटफ्लिक्स मैराथन (रोकने योग्य)
 // @namespace          https://github.com/aminomancer
-// @version            5.7.3
+// @version            5.7.4
 // @description        A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:en     A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:zh-CN  一个可配置的脚本，可自动跳过重述、介绍、演职员表和广告，并点击 Netflix、Amazon Prime Video、Hulu、HBO Max、Starz、Disney+ 和 Hotstar 上的“下一集”提示。 可自定义的热键暂停/恢复自动跳过功能。 Alt + N 进行设置。
@@ -394,7 +394,8 @@ const methods = {
   // skip stuff. when the script is not paused, they are invoked on a timer.
   async amazon() {
     if (this.count === 0) {
-      if (this.byID("dv-web-player").offsetParent) {
+      const player = this.byID("dv-web-player");
+      if (player && player.offsetParent) {
         let store; // memoize the element when we check for its existence so we don't have to evaluate the DOM twice.
         if (
           (store = this.qry(".atvwebplayersdk-nextupcard-button")) &&
