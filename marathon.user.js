5313b6963af5a228841dc35e0316899e6deb12df
Shane Hughes <shughes@mozilla.com>
2025-03-31 01:21:47 -0700

    5.7.9 - fix hbo max clicks

--- marathon.user.js
+++ marathon.user.js
@@ -10,7 +10,7 @@
 // @name:ru            Netflix Marathon (пауза)
 // @name:hi            नेटफ्लिक्स मैराथन (रोकने योग्य)
 // @namespace          https://github.com/aminomancer
-// @version            5.7.8
+// @version            5.7.9
 // @description        A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:en     A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:zh-CN  一个可配置的脚本，可自动跳过重述、介绍、演职员表和广告，并点击 Netflix、Amazon Prime Video、Hulu、HBO Max、Starz、Disney+ 和 Hotstar 上的“下一集”提示。 可自定义的热键暂停/恢复自动跳过功能。 Alt + N 进行设置。
@@ -557,7 +557,7 @@ const methods = {
         ))
       ) {
         // skip intro, skip recap, skip ad, etc.
-        this.maxPress(store);
+        this.clk(store);
         return;
       }
       if (
@@ -577,7 +577,7 @@ const methods = {
           nextEpisode?.episodeNumber || // tv series
           (options.promoted && nextEpisode?.id) // promoted film/series
         ) {
-          this.maxPress(store);
+          this.clk(store);
           return;
         }
       }
